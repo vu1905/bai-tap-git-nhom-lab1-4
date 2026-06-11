6f27cc081874186ee4a806db38d11c216a1cd60b
toan0309 <46134381+toan0309@users.noreply.github.com>
2026-06-11 14:35:43 +0700

    feat: sua lai dung cong thuc tinh gpa theo he so 4

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ def tinh_diem_gpa(diem_so):
     if diem_so >= 8.5:
         return 4.0
     else:
-        return round((diem_so / 10) * 3.5, 2)
+        return round((diem_so / 10) * 4, 2)
 
 print("Điểm GPA hệ 4 là:", tinh_diem_gpa(8.5))
